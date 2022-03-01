b893fd125628f9a9ad4c8d36c6bc4007124cfed2
Nicole Steadman <nsteadman@gmail.com>
2022-03-01 11:53:30 -0800

    commit tree 6f7368aa6ee3029b9951d760edd02f82c628569c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+    name_array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(speakers_array)
+    speakers_array.map.with_index(1) do |speaker, room_number|
+        "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer(speakers_array)
+    batch_badge_creator(speakers_array).each do |speaker|
+        puts speaker
+    end
+    assign_rooms(speakers_array).each do |room_number|
+        puts room_number
+    end
+end
